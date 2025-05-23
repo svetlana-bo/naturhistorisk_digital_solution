1c22e7492fa93d0a338c0ff72608b4ce614cc2a0
svetlana-bo <eaa24svbo@students.eaaa.dk>
2025-05-23 14:06:10 +0200

    Default page fixes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,16 @@
 import styles from '../modules/Title.module.css';
 
 const Title = () => {
-    return <h1 className={styles.title1}>
-        Rituals in Nature: 
-        <span className={styles.title2}> 
+    return (
+    <>
+        <h1 className={styles.title1}>
+        Rituals of Nature: 
+        </h1>
+        <h2 className={styles.title2}> 
         Consent in Paradise
-        </span>
-        </h1>;
+        </h2>
+    </>
+    );
   };
   
   export default Title;
