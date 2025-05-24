6b67aad92485636ca433a7995c0666dd1f228e74
svetlana-bo <eaa24svbo@students.eaaa.dk>
2025-05-24 21:43:12 +0200

    404 styled

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,5 +1,14 @@
+import styles from '../modules/NotFound.module.css';
+import egg from '../assets/images/egg.png';
+
 function NotFound() {
-    return <h1>404 - Not Found</h1>;
+      return (
+      <div className={styles.main_container}>
+      <h1>404</h1>
+      <h2>Something went wrong</h2>
+      <img src={egg} alt="Not Found" className={styles.image} />
+      </div>
+    )
   }
   
   export default NotFound;
